f82192d6b0baa974abf44d90d392a830134807b5
na <kanapodream@gmail.com>
2021-08-17 11:46:29 +0900

    update

--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 public class Sample : MonoBehaviour
 {
     // Start is called before the first frame update
     async void Start()
     {
-        var cube = await Addressables.InstantiateAsync("Assets/Prefab/Cube.prefab"); 
-    }
+        var www = UnityWebRequestAssetBundle.GetAssetBundle("http://192.168.1.47:5500/scene");
 
-    private async UniTask Wait()
-    {
-        Debug.Log("a");
-        await Task.Delay(5000);
-        Debug.Log("b");
+        await www.SendWebRequest();
+        
+        if(www.result == UnityWebRequest.Result.Success) {
+            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(www);
+            
+            if (!bundle.isStreamedSceneAssetBundle)
+            {
+                await bundle.LoadAllAssetsAsync();
+            }
+            
+            Debug.Log(bundle.GetAllScenePaths().Length);
+            
+            var sceneName = Path.GetFileNameWithoutExtension(bundle.GetAllScenePaths()[0]);
+            SceneManager.LoadScene(sceneName);
+        }
+        else {
+            Debug.Log(www.error);
+        }
     }
 }
--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 2. Window/アセット管理/Addressable/で確認
 
 GameObject obj = await Addressables.InstantiateAsync("アドレス")
+
+https://qiita.com/nisoka/items/66308f5d1029c1f29253
